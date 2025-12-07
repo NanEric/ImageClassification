3eec812f9b2f08bad03066baf8f1b4efe69a4cf0
eric <eric@192.168.10.21>
2025-12-07 13:25:36 +0800

    Initial commit

---
+++ ImageClassification/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ImageClassification
+//
+//  Created by eric on 2025/12/7.
+//
+
+import UIKit
+import CoreML
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
---
+++ ImageClassificationTests/ImageClassificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationTests.swift
+//  ImageClassificationTests
+//
+//  Created by eric on 2025/12/7.
+//
+
+import Testing
+@testable import ImageClassification
+
+struct ImageClassificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
